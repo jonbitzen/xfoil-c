2f43e470d4079803824fceebe255ce6509628334
Jonathan Railsback <jonbitzen@hotmail.com>
2025-06-14 15:41:18 +0000

    removed the initial go at converting this stuff to C++ - too much work

--- include/xfoil/xfoil_lib.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace xfoil {
-    int add(int x, int y);
-};
--- source/xfoil_lib.cc
+++
@@ -1,7 +0,0 @@
-#include "xfoil/xfoil_lib.h"
-
-namespace xfoil {
-    int add(int x, int y) {
-        return x+y;
-    }
-};
